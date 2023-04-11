67009ad573adf34ee45aace0f104d88a0394f3dd
21ftt1753 <21ftt1753@student.pb.edu.bn>
2023-04-11 12:20:38 +0800

    updated files

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,15 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+	<div class="flex flex-col max-w-md p-6 dark:bg-gray-900 dark:text-gray-100">
+		<img src="\src\assets\apik.jpg" alt="" class="flex-shrink-0 object-cover h-64 rounded-sm sm:h-96 dark:bg-gray-500 aspect-square">
+		<div>
+      <br>
+			<h2 class="text-xl font-semibold"> Afiq Aiman </h2>
+			<span class="block pb-2 text-sm dark:text-gray-400"> DWDT07 </span>
+			<span class="block pb-2 text-sm dark:text-gray-400"> 21ftt1753 </span>
+			<p>I'm One of The Politeknik Brunei Students Pursuing Level 5 Diploma in Web Development.</p>
+		</div>
+	</div>
 </template>
 
-<!-- <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style> -->
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/21ftt1753-as3/',
-  plugins: [vue()]
+  plugins: [vue()],
 })
